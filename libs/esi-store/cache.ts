6da13b5eaa9130a0c53e01dd45474f47e000f954
raph5 <raphguyader@gmail.com>
2024-12-30 16:15:51 +0100

    cleanup work

--- libs/esi-store/cache.ts
+++
@@ -1,9 +0,0 @@
-import { readFile, writeFile } from "node:fs/promises"
-
-export async function readCacheFile(cacheFolder: string, cacheFile: string): Promise<string | null> {
-  return await readFile(`${cacheFolder}/${cacheFile}.json`, { encoding: 'utf8' }).catch(() => null)
-}
-
-export async function writeCacheFile(cacheFolder: string, cacheFile: string, text: string) {
-  await writeFile(`${cacheFolder}/${cacheFile}.json`, text, { encoding: 'utf8' }).catch()
-}
--- libs/utils/server.ts
+++
@@ -1,5 +0,0 @@
-import csv from "async-csv";
-
-export function parseCsv<T=any>(csvData: string): Promise<T>  {
-  return csv.parse(csvData, { columns: true }) as Promise<T>
-}
