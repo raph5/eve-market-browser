347147d7dd61de62cccad92323091a5e7542f7c9
raph5 <raphguyader@gmail.com>
2025-03-21 16:33:41 +0100

    fix

--- apps/store/items/timerecord/timerecord.go
+++ apps/store/items/timerecord/timerecord.go
@@ -5,15 +5,17 @@ import (
 	"database/sql"
 	"errors"
 	"time"
+
+	"github.com/raph5/eve-market-browser/apps/store/lib/database"
 )
 
 func Set(ctx context.Context, key string, expTime time.Time) error {
-	writeDB := ctx.Value("writeDB").(*sql.DB)
+	db := ctx.Value("db").(*database.DB)
 	timeoutCtx, cancel := context.WithTimeout(ctx, 5*time.Minute)
 	defer cancel()
 
 	insertQuery := "INSERT OR REPLACE INTO TimeRecord VALUES (?,?)"
-	_, err := writeDB.ExecContext(timeoutCtx, insertQuery, key, expTime.Unix())
+	_, err := db.Exec(timeoutCtx, insertQuery, key, expTime.Unix())
 	if err != nil {
 		return err
 	}
@@ -22,13 +24,13 @@ func Set(ctx context.Context, key string, expTime time.Time) error {
 }
 
 func Get(ctx context.Context, key string) (time.Time, error) {
-	readDB := ctx.Value("readDB").(*sql.DB)
+	db := ctx.Value("db").(*database.DB)
 	timeoutCtx, cancel := context.WithTimeout(ctx, 5*time.Minute)
 	defer cancel()
 
 	var unixTime int64
 	selectQuery := `SELECT "Time" FROM TimeRecord WHERE "Key" = ?`
-	err := readDB.QueryRowContext(timeoutCtx, selectQuery, key).Scan(&unixTime)
+	err := db.QueryRow(timeoutCtx, selectQuery, key).Scan(&unixTime)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			return time.Now().Add(-1 * time.Second), nil
