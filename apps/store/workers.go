da209699c3a426f7fb1ad2d3545ce71a55ec1398
raph5 <raphguyader@gmail.com>
2026-02-25 16:53:02 +0100

    fix

--- apps/store/workers.go
+++ apps/store/workers.go
@@ -92,7 +92,7 @@ func historyWorker(ctx context.Context) {
 
 	if len(activeMarkets) == 0 {
 		activeMarkets, err := dbGetActiveMarkets(ctx)
-		sleepWithContext(ctx, 15*time.Minute)
+		sleepWithContext(ctx, 20 * time.Minute)
 		if err != nil {
 			log.Printf("Hisotry Worker Error: initial dbGetActiveMarkets: %v", err)
 			return
